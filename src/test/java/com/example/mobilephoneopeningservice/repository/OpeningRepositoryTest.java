7b4e2ade7c3f1c4b4a67740cf9dc2a0e2f22c4e1
seungyeon.lee <sylee5752@naver.com>
2021-11-11 16:12:48 +0900

    add junit test code(OpeningRepositoryTest), add html sources

--- src/test/java/com/example/mobilephoneopeningservice/repository/OpeningRepositoryTest.java
+++ src/test/java/com/example/mobilephoneopeningservice/repository/OpeningRepositoryTest.java
@@ -5,7 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.stereotype.Repository;
 
+import com.example.mobilephoneopeningservice.domain.Opening;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDateTime;
 @SpringBootTest
 class OpeningRepositoryTest {
 
@@ -14,9 +18,22 @@ class OpeningRepositoryTest {
 
     @Autowired
     OpeningHistoryRepository openingHistoryRepository;
+    
+    @Autowired
+    CustomerRepository customerRepository;
 
     @Test
     void openingCrud(){
+    	openingRepository.findAll().forEach(System.out::println);
+    	Opening opening = new Opening();
+    	opening.setCustomer(customerRepository.findById(1L).orElse(null));
+    	opening.setRequestDt(LocalDateTime.now());
+    	opening.setStatus(1);
+    	openingRepository.save(opening);
+    	
+    	openingRepository.findAll().forEach(System.out::println);
+    	
+    	openingHistoryRepository.findAll().forEach(System.out::println);
 
     }
 }
