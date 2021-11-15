054f4b080f5496da5f0c74cd1d12450e9452493b
seungyeon.lee <sylee5752@naver.com>
2021-11-16 01:07:45 +0900

    Merge remote-tracking branch 'origin/master'

--- src/main/java/com/example/mobilephoneopeningservice/repository/CustomerRepository.java
+++ src/main/java/com/example/mobilephoneopeningservice/repository/CustomerRepository.java
@@ -7,8 +7,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-	
+
+	List<Customer> findByName(String name);
+
 	Page<Customer> findAll(Pageable pageable);
 }
